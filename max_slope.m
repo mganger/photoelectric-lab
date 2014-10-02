01395bbf8ed8b6453b2be788d50cccff56fc8f14
Michael Ganger <mganger747@gmail.com>
2014-10-02 03:14:57 -0400

    latest solution

--- max_slope.m
+++ max_slope.m
@@ -1,7 +1,7 @@
 initial = @(x) (c * atan(b * a.^x).^d + e)
 inverse = @(x) (log(tan(((x-e)/c).^(1/d))/b)/log(a))
 slope = @(x) ((a.^x * b * c * d * atan(a.^x * b).^(d-1) * log(a))/(1 + a.^(2*x) * b.^2))
-h = (pi/2).^d * c
+h = (pi/2).^d * c / 2
 target = percent*h + c
 
 [minx, minSlope] = fminbnd(slope, -10, 10)
