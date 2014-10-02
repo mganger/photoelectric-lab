f359e228814fdb70c1a65c1b18d81d6c80a89b60
Michael Ganger <mganger747@gmail.com>
2014-10-01 21:41:54 -0400

    got the uncertainty associated with each of the measurements

--- max_slope.m
+++ max_slope.m
@@ -1,8 +1,16 @@
 initial = @(x) (c * atan(b * a.^x).^d + e)
+inverse = @(x) (log(tan(((x-e)/c).^(1/d))/b)/log(a))
 slope = @(x) ((a.^x * b * c * d * atan(a.^x * b).^(d-1) * log(a))/(1 + a.^(2*x) * b.^2))
+h = (pi/2).^d * c
+target = percent*h + c
 
 [minx, minSlope] = fminbnd(slope, -10, 10)
 
 intercept = initial(minx) - minSlope * minx
 
 stoppingPotential = (e - intercept)/minSlope
+
+leftBound = minx
+rightBound = abs(abs(inverse(target)))
+
+uncertainty = (rightBound + leftBound)/2
